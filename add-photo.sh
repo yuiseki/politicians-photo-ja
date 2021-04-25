a666cff0c6b43e5cc9d0605fee9aac441de99d97
yuiseki <yuiseki@gmail.com>
2021-04-25 20:55:29 +0900

    add wikidata_id file

--- add-photo.sh
+++ add-photo.sh
@@ -4,16 +4,30 @@
 _add_photo () {
   for name in $(cat -)
   do
+    echo -n
     echo $name
-    id=$(wb id $name -l ja)
-    echo $id
-    url=$(wb query -s $id -p P18)
-    echo $url
-    filename=$(basename $url)
-    filename=${filename//%20/_}
     first=$(node add.js $name)
     echo $first
-    wget -q -O public/politicians/${first:0:1}/${first}/$name/$filename $url
+    dirpath="public/politicians/${first:0:1}/${first}/$name"
+    echo $dirpath
+    if [ -e $dirpath/wikidata_id ]; then
+      id=$(cat $dirpath/wikidata_id)
+    else
+      id=$(wb id $name -l ja)
+      echo $id > "$dirpath/wikidata_id"
+    fi
+    echo $id
+    if [ -z "$id" ]; then
+      continue
+    fi
+    photourl=$(wb query -s $id -p P18)
+    echo $photourl
+    if [ -z "$photourl" ]; then
+      continue
+    fi
+    filename=$(basename $photourl)
+    filename=${filename//%20/_}
+    wget -q -O $dirpath/$filename $photourl
   done
 }
 
